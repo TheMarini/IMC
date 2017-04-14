8991d9e98191471cd01f215674522928555e4719
Bruno Marini <bruno.marini@hotmail.com>
2017-04-14 00:08:09 -0300

    the project itself (I've already done)

---
+++ prjFormClasse/frmFormClasse.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace prjFormClasse
+{
+    partial class frmFormClasse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbtnMasc = new System.Windows.Forms.RadioButton();
+            this.rbtnFem = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pcbImagem = new System.Windows.Forms.PictureBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(57, 35);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 0;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(57, 61);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 1;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Altura:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Peso:";
+            // 
+            // rbtnMasc
+            // 
+            this.rbtnMasc.AutoSize = true;
+            this.rbtnMasc.Location = new System.Drawing.Point(8, 19);
+            this.rbtnMasc.Name = "rbtnMasc";
+            this.rbtnMasc.Size = new System.Drawing.Size(34, 17);
+            this.rbtnMasc.TabIndex = 4;
+            this.rbtnMasc.TabStop = true;
+            this.rbtnMasc.Text = "M";
+            this.rbtnMasc.UseVisualStyleBackColor = true;
+            this.rbtnMasc.CheckedChanged += new System.EventHandler(this.rbtnMasc_CheckedChanged);
+            // 
+            // rbtnFem
+            // 
+            this.rbtnFem.AutoSize = true;
+            this.rbtnFem.Location = new System.Drawing.Point(8, 42);
+            this.rbtnFem.Name = "rbtnFem";
+            this.rbtnFem.Size = new System.Drawing.Size(31, 17);
+            this.rbtnFem.TabIndex = 5;
+            this.rbtnFem.TabStop = true;
+            this.rbtnFem.Text = "F";
+            this.rbtnFem.UseVisualStyleBackColor = true;
+            this.rbtnFem.CheckedChanged += new System.EventHandler(this.rbtnFem_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAltura);
+            this.groupBox1.Controls.Add(this.txtPeso);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(175, 99);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbtnMasc);
+            this.groupBox2.Controls.Add(this.rbtnFem);
+            this.groupBox2.Location = new System.Drawing.Point(193, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(47, 69);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sexo";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Enabled = false;
+            this.btnCalc.Location = new System.Drawing.Point(16, 7);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(95, 35);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "&Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(149, 7);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(95, 35);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnCalc);
+            this.panel1.Controls.Add(this.btnLimpar);
+            this.panel1.Location = new System.Drawing.Point(-4, 250);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 50);
+            this.panel1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(103, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "∞ ∞ ∞";
+            // 
+            // pcbImagem
+            // 
+            this.pcbImagem.Location = new System.Drawing.Point(14, 116);
+            this.pcbImagem.Name = "pcbImagem";
+            this.pcbImagem.Size = new System.Drawing.Size(225, 91);
+            this.pcbImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbImagem.TabIndex = 13;
+            this.pcbImagem.TabStop = false;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblRes.ForeColor = System.Drawing.Color.Gray;
+            this.lblRes.Location = new System.Drawing.Point(65, 207);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(141, 20);
+            this.lblRes.TabIndex = 12;
+            this.lblRes.Text = "Insira seus dados";
+            // 
+            // txtIMC
+            // 
+            this.txtIMC.Enabled = false;
+            this.txtIMC.Location = new System.Drawing.Point(193, 16);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(47, 20);
+            this.txtIMC.TabIndex = 14;
+            this.txtIMC.Text = "IMC";
+            this.txtIMC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.toolTip1.SetToolTip(this.txtIMC, "valor IMC");
+            // 
+            // frmFormClasse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 297);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.pcbImagem);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "frmFormClasse";
+            this.Text = "IMC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmFormClasse_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbtnMasc;
+        private System.Windows.Forms.RadioButton rbtnFem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pcbImagem;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
